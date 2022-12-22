6b3e786070906c12fb02d247c0ebba026ae493fb
bhattsachin941712 <120157604+bhattsachin941712@users.noreply.github.com>
2022-12-22 19:31:08 +0530

    Delete Assignment 2 directory

--- Assignment 2/Q2.py
+++
@@ -1,5 +0,0 @@
-name="Hitesh Kochar"
-sid=22104074
-dept="Electrical"
-cg=10.0
-print(" Hey,",name,"Here!\n","My SID is",sid,"\n","I am from",dept,"Department and my CGPA is",cg)
--- Assignment 2/Q5.py
+++
@@ -1,6 +0,0 @@
-a=input("Enter something: ")
-b=a.find("name")
-if b==-1:
-    print("No")
-else:
-    print("Yes")
